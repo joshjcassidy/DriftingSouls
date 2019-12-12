daefe365ecba1e3c97f59f29f9a1f4d7de4d54a4
Amaterixen <joshjcassidy@hotmail.com>
2019-12-12 22:48:16 +0000

    Started development, added walking and semi-implemented box-pushing

---
+++ Assets/PlayerPush.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPush : MonoBehaviour
+{
+    public float distance = 1f;
+    public LayerMask boxMask;
+
+    GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.queriesStartInColliders = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+
+        if (hit.collider != null && Input.GetKeyDown(KeyCode.E))
+        {
+            box = hit.collider.gameObject;
+        }
+    }
+
+        void OnDrawGizmos(){
+            Gizmos.color = Color.yellow;
+
+            Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+        }
+}
